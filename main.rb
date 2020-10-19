31c80081be0476dd58facb46bf29276e5c6f23a0
Victor Maslov <nakilon@gmail.com>
2020-10-19 03:14:00 +0300

    the first table added -- 'Large Text Compression Benchmark'

---
+++ main.rb
@@ -0,0 +1,14 @@
+require "set"
+require "pcbr"
+Dir.glob("*.txt").map do |filename|
+  title, description, directions, *s = File.read(filename).split(?\n)
+  puts "### #{title}\n(#{description})\n|points||\n|-|-"
+  pcbr = PCBR.new
+  s.map{ |_| _.ljust s.map(&:size).max, ?\s }.map(&:chars).transpose.chunk{ |_| _.uniq == [" "] }.reject(&:first).map{ |_| _.last.transpose.map &:join }.transpose.each do |item, *vector|
+    pcbr.store item.strip, vector.zip(directions.split).map{ |value, direction| direction == ?- ? -Rational(value) : Rational(value) unless value.strip.empty? }
+  end
+  pcbr.table.sort_by(&:last).reverse.chunk(&:last).each_with_index do |(score, g), i|
+    puts "|#{score}|#{g.map(&:first).join(", ")}"
+  end
+  puts "---"
+end
