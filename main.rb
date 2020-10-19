6f0a71fec5da7d6d6d02813807703542d4cdfa9f
Victor Maslov <nakilon@gmail.com>
2020-10-19 05:51:40 +0300

    added the 'Imgur alternatives' table

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require "set"
 require "pcbr"
 Dir.glob("*.txt").map do |filename|
-  title, description, directions, *s = File.read(filename).split(?\n)
-  puts "### #{title}\n(#{description})\n|points||\n|-|-"
+  title, description, _, directions, *s = File.read(filename).split(?\n)
+  puts "## #{title}\n(#{description})\n|points||\n|-|-"
   pcbr = PCBR.new
   s.map{ |_| _.ljust s.map(&:size).max, ?\s }.map(&:chars).transpose.chunk{ |_| _.uniq == [" "] }.reject(&:first).map{ |_| _.last.transpose.map &:join }.transpose.each do |item, *vector|
     pcbr.store item.strip, vector.zip(directions.split).map{ |value, direction| direction == ?- ? -Rational(value) : Rational(value) unless value.strip.empty? }
