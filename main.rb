544089b1898244db797b657a7ef36e5f4e016a9c
Victor Maslov <nakilon@gmail.com>
2020-10-23 15:03:22 +0300

    added the 'happy/cursing' programming languages ranking from my old gist

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require "set"
 require "pcbr"
 Dir.glob("*.txt").map do |filename|
   title, description, _, directions, *s = File.read(filename).split(?\n)
-  puts "## #{title}\n(#{description})\n|points||\n|-|-"
+  puts "## #{title}\n(#{description})\n|score||\n|-|-"
   pcbr = PCBR.new
   s.map{ |_| _.ljust s.map(&:size).max, ?\s }.map(&:chars).transpose.chunk{ |_| _.uniq == [" "] }.reject(&:first).map{ |_| _.last.transpose.map &:join }.transpose.each do |item, *vector|
     pcbr.store item.strip, vector.zip(directions.split).map{ |value, direction| direction == ?- ? -Rational(value) : Rational(value) unless value.strip.empty? }
