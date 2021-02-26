894a5da73f2860882ca51e516fa11d2a04de1de1
Victor Maslov <nakilon@gmail.com>
2021-02-26 13:25:48 +0300

    .txt files now can have comments (for columns or whatever you want for now)

--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
 require "set"
 require "pcbr"
 Dir.glob("*.txt").sort.map do |filename|
-  title, description, _, directions, *s = File.read(filename).split(?\n)
+  fail unless /\A(?<title>.+)\n(?<description>.+)\n(\S.*\n)*( .+)\n(?<directions> .+)\n/ =~ File.read(filename)
+  s = $'.split "\n"
   puts "## #{title}\n(#{description})\n|score||\n|-|-"
   pcbr = PCBR.new
   s.map{ |_| _.ljust s.map(&:size).max, ?\s }.map(&:chars).transpose.chunk{ |_| _.uniq == [" "] }.reject(&:first).map{ |_| _.last.transpose.map &:join }.transpose.each do |item, *vector|
     pcbr.store item.strip, vector.zip(directions.split).map{ |value, direction| direction == ?- ? -Rational(value) : Rational(value) unless value.strip.empty? }
   end
   pcbr.table.sort_by(&:last).chunk(&:last).reverse_each do |score, g|
-    puts "|#{score}|#{g.map(&:first).join(", ")}"
+    puts "|#{score}|#{g.map(&:first).sort.join(", ")}"
   end
   puts "---"
 end
-
